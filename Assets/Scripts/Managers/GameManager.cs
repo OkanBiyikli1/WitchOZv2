14c37f0760c54a652c6b99ad58dcd11619fdb09a
Okan <160130699+OkanBiyikli1@users.noreply.github.com>
2024-06-07 17:02:34 +0300

    LineAdded

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -17,8 +17,14 @@ public class GameManager : MonoBehaviour
     [SerializeField] List<Customers> customers;
     [SerializeField] private GameObject customer;
     [SerializeField] private TextMeshProUGUI customerText, customerMoveText;
-    [SerializeField] private int customerIndex = 0;
+    [SerializeField] private int levelIndex = 0;
     [SerializeField] private TextMeshProUGUI dialogText;
+
+    [Header("UI")]
+
+    [SerializeField] private GameObject line2;
+    [SerializeField] private GameObject line3;
+    [SerializeField] private TextMeshProUGUI currentLevelText;
 
     void Awake()
     {
@@ -30,17 +36,21 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
-        GetNextCustomer(customerIndex);
+        CheckLineToActivate();
+        GetNextCustomer(levelIndex);
         containerText.text = potionAmount.ToString();
-        dialogText.text = customers[customerIndex].dialog;
+        dialogText.text = customers[levelIndex].dialog;
+        currentLevelText.text = "Level: " + levelIndex.ToString();
     }
 
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.N))
         {
-            customerIndex++;
-            GetNextCustomer(customerIndex);
+            levelIndex++;
+            GetNextCustomer(levelIndex);
+            currentLevelText.text = "Level: " + levelIndex.ToString();
+            CheckLineToActivate();
         }
     }
 
@@ -52,19 +62,21 @@ public class GameManager : MonoBehaviour
 
     public void Generate()
     {
-        if(potionAmount == customers[customerIndex].neededAmount)
+        if(potionAmount == customers[levelIndex].neededAmount)
         {
             potionAmount = 0;
-            customerIndex++;
-            GetNextCustomer(customerIndex);
+            levelIndex++;
+            GetNextCustomer(levelIndex);
             UpdateContainerText();
             Debug.Log("iyilestirdin");
+            CheckLineToActivate();
+            currentLevelText.text = "Level: " + levelIndex.ToString();
         }
-        else if(potionAmount < customers[customerIndex].neededAmount)
+        else if(potionAmount < customers[levelIndex].neededAmount)
         {
             Debug.Log("değeri arttirmalisin");
         }
-        else if(potionAmount > customers[customerIndex].neededAmount)
+        else if(potionAmount > customers[levelIndex].neededAmount)
         {
             Debug.Log("değeri azaltmalisin");
         }
@@ -72,10 +84,19 @@ public class GameManager : MonoBehaviour
 
     public void GetNextCustomer(int index)
     {
-        customerText.text = customers[customerIndex].neededAmount.ToString();
-        customerMoveText.text = customers[customerIndex].moveValue.ToString();
-        playerMove = customers[customerIndex].moveValue;
-        dialogText.text = customers[customerIndex].dialog;
+        customerText.text = customers[levelIndex].neededAmount.ToString();
+        customerMoveText.text = customers[levelIndex].moveValue.ToString();
+        playerMove = customers[levelIndex].moveValue;
+        dialogText.text = customers[levelIndex].dialog;
+    }
+
+    void CheckLineToActivate()
+    {
+        if(levelIndex == 25)
+        line2.SetActive(true);
+
+        if(levelIndex == 50)
+        line3.SetActive(true);
     }
 
 }
